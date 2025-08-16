5b85000bda54bd6ac44393746ae08bdf98b1cacf
KK <kk@MacBook-Pro-khxngKraingkrai.local>
2025-08-16 13:15:33 +0700

    chore: add spider

--- init.lua
+++ init.lua
@@ -73,6 +73,21 @@ if vim.g.vscode then
         })
       end,
     },
+    
+    -- Spider - smart word movement
+    {
+      "chrisgrieser/nvim-spider",
+      keys = {
+        { "w", function() require('spider').motion('w') end, mode = { "n", "o", "x" }, desc = "Spider-w" },
+        { "e", function() require('spider').motion('e') end, mode = { "n", "o", "x" }, desc = "Spider-e" },
+        { "b", function() require('spider').motion('b') end, mode = { "n", "o", "x" }, desc = "Spider-b" },
+      },
+      config = function()
+        require("spider").setup({
+          skipInsignificantPunctuation = true
+        })
+      end,
+    },
   })
   
   -- VS Code keymaps - minimal
